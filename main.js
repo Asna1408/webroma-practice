763ae43529a8455863f89c55bbe340e2f80b546f
Asna1408 <85230276+Asna1408@users.noreply.github.com>
2021-08-21 12:49:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let count=0;
+function increment(){
+  let val=document.getElementById("num").innerHTML;
+  console.log(val)
+  let num=Number(val)+1
+  document.getElementById("num").innerHTML=num;
+}
+function decrement(){
+    let val=document.getElementById("num").innerHTML;
+    let num=Number(val)-1
+    document.getElementById("num").innerHTML=num;
+  }
